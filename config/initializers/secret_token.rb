06b6fc14ffbe8cb69df2ae8fd1e692ab11618108
Matt Beedle <mattbeedle@googlemail.com>
2014-01-20 16:48:05 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Portfolio::Application.config.secret_key_base = '147c04d670002725a8ccf6d038f217941a36b10b7683cebc9be039fea53afeffb7cb099c1108bc2b6f0f19416a48407746314aae6accae3d69d8c48ea1e0bf0b'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+Portfolio::Application.routes.draw do
+  root to: 'pages#index'
+end
