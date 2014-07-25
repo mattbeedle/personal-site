a1baa0c11144e69088ab91e1cf8b56914d8eb8e8
Matt Beedle <mattbeedle@googlemail.com>
2014-07-25 11:20:03 +0200

    All reverse proxy routes working

--- config/initializers/rack_reverse_proxy.rb
+++ config/initializers/rack_reverse_proxy.rb
@@ -1,4 +1,6 @@
 Rails.application.config.middleware.insert_before ActionDispatch::Static, Rack::ReverseProxy do
+  reverse_proxy_options matching: :first
+
   reverse_proxy %r{^\/blog(\/?.*)$}, "#{ENV['BLOG_HOST']}$1"
   reverse_proxy '/css', ENV['BLOG_HOST']
   reverse_proxy '/posts', ENV['BLOG_HOST']
