a9415864eed58b65f978d87a68889c3c2726eeb5
Matt Beedle <mattbeedle@googlemail.com>
2014-07-25 10:38:51 +0200

    reverse proxy /posts

--- config/initializers/rack_reverse_proxy.rb
+++ config/initializers/rack_reverse_proxy.rb
@@ -1,4 +1,5 @@
-Rails.application.config.middleware.insert_before Rack::Lock, Rack::ReverseProxy do
+Rails.application.config.middleware.insert_before ActionDispatch::Static, Rack::ReverseProxy do
   reverse_proxy %r{^\/blog(\/?.*)$}, "#{ENV['BLOG_HOST']}$1"
   reverse_proxy '/css', ENV['BLOG_HOST']
+  reverse_proxy '/posts', ENV['BLOG_HOST']
 end
